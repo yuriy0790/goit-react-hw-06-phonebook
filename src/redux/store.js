6c455429e2ae585d6e6cdc0c11916267fa750881
Yurii Oborskiy <yuriioborskiy@gmail.com>
2023-01-20 21:34:19 +0200

    feat: add filter to local storage blacklist

--- src/redux/store.js
+++ src/redux/store.js
@@ -22,6 +22,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: 'root',
   storage,
+  blacklist: ['filter'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
