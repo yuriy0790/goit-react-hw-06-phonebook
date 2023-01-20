87393a06f510e9831accbf27f44efba43ba6bd7d
Yurii Oborskiy <yuriioborskiy@gmail.com>
2023-01-20 14:48:04 +0200

    feat: create store

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+// import { todosReducer } from './todos/todosSlice';
+// import { filterReducer } from './filterSlice';
+// import { friendsReducer } from './friends/friendsSlice';
+
+export const store = configureStore({
+  reducer: {
+    // todos: todosReducer,
+    // friends: friendsReducer,
+    // filter: filterReducer,
+  },
+});
